6409bf37c649790a5b092831dc4660a2606c7141
ijnim1121 <jimin3347@naver.com>
2025-03-10 22:09:02 +0900

    feat: redis pub/sub

--- src/main/java/eightplusone/bit/fit/global/config/RedisConfig.java
+++ src/main/java/eightplusone/bit/fit/global/config/RedisConfig.java
@@ -5,8 +5,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.listener.PatternTopic;
+import org.springframework.data.redis.listener.RedisMessageListenerContainer;
+import org.springframework.data.redis.listener.adapter.MessageListenerAdapter;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import eightplusone.bit.fit.global.pubsub.ChatSubscriber;
 
 @Configuration
 public class RedisConfig {
@@ -20,7 +24,23 @@ public class RedisConfig {
 		RedisTemplate<String, Object> template = new RedisTemplate<>();
 		template.setConnectionFactory(redisConnectionFactory());
 		template.setKeySerializer(new StringRedisSerializer());
-		template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+		template.setValueSerializer(new StringRedisSerializer());
 		return template;
+	}
+
+	// Redis 메시지 리스너 등록
+	@Bean
+	public RedisMessageListenerContainer redisContainer(RedisConnectionFactory connectionFactory,
+		MessageListenerAdapter listenerAdapter) {
+		RedisMessageListenerContainer container = new RedisMessageListenerContainer();
+		container.setConnectionFactory(connectionFactory);
+		container.addMessageListener(listenerAdapter, new PatternTopic("chat-room")); // "chat-room" 채널 구독
+		return container;
+	}
+
+	// ChatSubscriber를 Redis 리스너로 등록
+	@Bean
+	public MessageListenerAdapter listenerAdapter(ChatSubscriber subscriber) {
+		return new MessageListenerAdapter(subscriber, "onMessage");
 	}
 }

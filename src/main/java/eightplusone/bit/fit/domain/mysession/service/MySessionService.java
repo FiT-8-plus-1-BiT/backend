68cf9e5e6c8539c15dc987bb78db4997a0ae06c0
조영무 <fprh13@gmail.com>
2025-03-18 10:14:25 +0900

    feat: 스케쥴을 위한 미리 담은 강연 리스트 응답 기능 #40

--- src/main/java/eightplusone/bit/fit/domain/mysession/service/MySessionService.java
+++ src/main/java/eightplusone/bit/fit/domain/mysession/service/MySessionService.java
@@ -1,9 +1,14 @@
 package eightplusone.bit.fit.domain.mysession.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import eightplusone.bit.fit.domain.mysession.dto.MySessionListResponseDto;
 import eightplusone.bit.fit.domain.mysession.entity.MySession;
+import eightplusone.bit.fit.domain.mysession.enums.MySessionType;
 import eightplusone.bit.fit.domain.mysession.repository.MySessionRepository;
 import eightplusone.bit.fit.domain.session.entity.Session;
 import eightplusone.bit.fit.domain.session.repository.SessionRepository;
@@ -28,5 +33,13 @@ public class MySessionService {
 		Session session = sessionRepository.findById(sessionId)
 			.orElseThrow(() -> new CustomException(ErrorCode.RESOURCE_NOT_FOUND));
 		mySessionRepository.save(MySession.register(user, session));
+	}
+
+	public List<MySessionListResponseDto> findRegisteredMySessions(String email) {
+		User user = userRepository.findLoginUserByEmail(email);
+		return mySessionRepository.findSessionsByUserIdAndType(user.getId(), MySessionType.REGISTER)
+			.stream()
+			.map(mySession -> MySessionListResponseDto.from(mySession.getSession()))
+			.collect(Collectors.toList());
 	}
 }

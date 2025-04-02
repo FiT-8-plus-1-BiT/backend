02d646f9d465527d225656f7fe2fbb85f925dbc6
조영무 <fprh13@gmail.com>
2025-04-02 16:55:38 +0900

    remove: 로그인 이후 소켓 테스트 디버깅 용 로그 삭제 #4

--- src/main/java/eightplusone/bit/fit/domain/auth/interceptor/WebSocketInterceptor.java
+++ src/main/java/eightplusone/bit/fit/domain/auth/interceptor/WebSocketInterceptor.java
@@ -2,11 +2,6 @@ package eightplusone.bit.fit.domain.auth.interceptor;
 
 import static eightplusone.bit.fit.global.constants.TokenConstant.*;
 
-import eightplusone.bit.fit.domain.auth.jwt.TokenProvider;
-import eightplusone.bit.fit.global.exception.CustomException;
-import eightplusone.bit.fit.global.exception.ErrorCode;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
@@ -15,6 +10,12 @@ import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.messaging.support.MessageHeaderAccessor;
 import org.springframework.security.core.Authentication;
+
+import eightplusone.bit.fit.domain.auth.jwt.TokenProvider;
+import eightplusone.bit.fit.global.exception.CustomException;
+import eightplusone.bit.fit.global.exception.ErrorCode;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Configuration
 @RequiredArgsConstructor
@@ -27,7 +28,6 @@ public class WebSocketInterceptor implements ChannelInterceptor {
 	public Message<?> preSend(Message<?> message, MessageChannel channel) {
 		StompHeaderAccessor accessor = MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
 
-		// accessor가 null일 경우 바로 통과
 		if (accessor == null) {
 			return message;
 		}
@@ -35,34 +35,30 @@ public class WebSocketInterceptor implements ChannelInterceptor {
 		StompCommand command = accessor.getCommand();
 
 		if (command == StompCommand.CONNECT) {
-			log.debug("🔗 WebSocket CONNECT 요청 허용");
 			return message;
 		}
 
 		if (command == StompCommand.SUBSCRIBE || command == StompCommand.SEND) {
 			String destination = accessor.getDestination();
-			log.debug("📩 STOMP {} 요청 - dest: {}", command, destination);
 
-			// 특정 경로 예외 처리
 			if (destination != null && destination.startsWith("/sub/session")) {
 				return message;
 			}
 
 			String authHeader = accessor.getFirstNativeHeader("Authorization");
 			if (authHeader == null) {
-				authHeader = accessor.getFirstNativeHeader("authorization"); //소문자도 확인
+				authHeader = accessor.getFirstNativeHeader("authorization");
 			}
 
-			// 토큰이 없거나 형식이 잘못된 경우 예외 처리
 			if (authHeader == null || !authHeader.startsWith(BEARER_PREFIX)) {
-				log.warn("❌ WebSocket 토큰 누락 또는 형식 오류 - 받은 값: {}", authHeader);
+				log.warn("잘못된 webSocket 토큰 헤더 값: {}", authHeader);
 				throw new CustomException(ErrorCode.INVALID_AUTH_TOKEN);
 			}
 
 			String accessToken = authHeader.substring(BEARER_PREFIX.length());
 
 			if (!tokenProvider.validateAccessToken(accessToken)) {
-				log.warn("❌ WebSocket 토큰 유효성 실패");
+				log.warn("webSocket 토큰 유효성 실패");
 				throw new CustomException(ErrorCode.INVALID_AUTH_TOKEN);
 			}
 
